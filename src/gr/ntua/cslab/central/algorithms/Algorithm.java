eb8e2fc01d07aeb8aeaee9541832ce86030dfabe
Giannis Giannakopoulos <giannisgiannak@gmail.com>
2012-06-07 11:01:58 +0300

    Added javadoc info

--- src/gr/ntua/cslab/central/algorithms/Algorithm.java
+++ src/gr/ntua/cslab/central/algorithms/Algorithm.java
@@ -5,37 +5,44 @@ import gr.ntua.cslab.central.database.Tuple;
 
 import java.util.ArrayList;
 
-
+/**
+ * Basic interface of the algorithms that are used for the anonymization. <br/>
+ * The qid paremeter is not set through this interface, as this info is given to the tuples.
+ * */
 public interface Algorithm {
+	/**
+	 * Method with which you can set the k parameter (as in k-anonymity).
+	 * @param k This is the k parameter given
+	 * */
+	public void setK(int k);
 	
-	public void run();
+	/**
+	 * Method that sets the initial data that are to be anonymized.
+	 * @param data	The actual data
+	 * */
+	public void setData(ArrayList<Tuple> data);
+	
 	/**
 	 * Method with which the current algorithm begins to run.
 	 * */
+	public void run();
 	
-	public ResultsArray<Tuple> getResults();
 	/**
 	 * Method with which the results of the algorithm are returned to the program.
+	 * @return The results of the anonymization
 	 * */
+	public ResultsArray<Tuple> getResults();
 	
-	public void setK(int k);
 	/**
-	 * Method with which you can set the k parameter (as in k-anonymity).
+	 * Method which allows to run an algorithm with debuggin mode (verbose mode).
+	 * @param Debug parameter (may be true ar false - default is false). 
 	 * */
-	
-	public void setData(ArrayList<Tuple> data);
-	/**
-	 * Method that sets the initial data that are to be anonymized.
-	 * */
-	
 	public void setDebug(boolean debug);
-	/**
-	 * Method which allows to run an algorithm with debuggin mode (verbose mode). 
-	 * */
 	
-	public ArrayList<Tuple> getInputData();
+	
 	/**
 	 * Returns the input data of the algorithms
+	 * @return The input data
 	 * */
-	
+	public ArrayList<Tuple> getInputData();	
 }
